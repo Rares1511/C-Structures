2a879d26da72e5615e831272437b59f9753ac05f
Rares1511 <raresteodorpopa15@gmail.com>
2024-10-15 10:13:46 +0300

    modified:   include/matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -7,7 +7,9 @@
 typedef xuniv_attr_t matrix_attr_t;
 
 typedef struct matrix {
-    vector *vec; 
+    vector *vec;        /*!< vector holding the (dimensions - 1) matrixes */
+    int dimensions;     /*!< number of dimensions of the matrix */
+    matrix_attr_t attr; /*!< attributes for the datatype inside the matrix */
 } matrix;
 
 #endif
